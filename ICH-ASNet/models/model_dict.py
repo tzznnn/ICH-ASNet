a2e39d53a7c4a73ae5f7836ed8eff6625087b471
tzznnn <nietianzong@qdu.edu.cn>
2025-02-22 22:38:53 +0800

    Delete ICH-ASNet directory

--- ICH-ASNet/models/model_dict.py
+++
@@ -1,5 +0,0 @@
-from models.segment_anything.build_sam import sam_model_registry
-
-def get_model():
-    model = sam_model_registry['vit_b'](checkpoint='./pretrained/sam_vit_b_01ec64.pth')
-    return model
